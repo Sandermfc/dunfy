0cfadf0470fb7bb5eba6299cf91a70c315a86b1f
Sandermfc <mathieu.robinson17@gmail.com>
2017-07-21 16:48:53 -0300

    Temporary testing logs added

--- popup.js
+++ popup.js
@@ -1,2 +1,20 @@
+// When the extension icon gets clicked (this script gets run):
+// 		execute content.js on the DOM of the active tab.
 
-chrome.extension.getBackgroundPage().addTheStuff();
+//TODO: REMOVE after done testing
+/*chrome.storage.sync.clear(function(){
+	alert("Cleared everything in storage.");
+});*/
+
+chrome.storage.sync.get("icon", function(items) {
+	alert(items["icon"]);
+});
+
+chrome.tabs.query({currentWindow:true, active:true}, function(tabs) {
+	var activeTab = tabs[0];
+	chrome.tabs.executeScript(activeTab.id, {file:"content.js"});
+});
+/*populatePopup();
+function populatePopup(){
+	console.log("populating popop.");
+}*/
