5577196742d2bf6c8b5a305767cc19d9a081f4e0
Sandermfc <mathieu.robinson17@gmail.com>
2017-07-21 16:47:50 -0300

    Highlight/click and use of chrome.storage
    After clicking the extension icon:
    - Hovering over an html element in any page will highlight it in yellow.
    - Cursor gets changed to crosshair.
    - When the user clicks:
          * get information relative to this page.
          * Save it in chrome.storage

---
+++ content.js
@@ -0,0 +1,53 @@
+alert("in content script");
+
+// Add a new "highlight" class to the current HTML page
+var style = document.createElement("style");
+style.id = "dunfy-temporary-style"
+style.type = "text/css";
+style.innerHTML = ".highlight:hover { background-color: yellow; }";
+style.innerHTML += "html { cursor: crosshair;}";
+document.getElementsByTagName('head')[0].appendChild(style);
+
+
+var prev;
+document.body.addEventListener('mouseover', highlightHover, false);
+document.body.addEventListener('click', tagClicked, false);
+
+function highlightHover(event) {
+	if (event.target === document.body || (prev && prev === event.target)) {
+		return;
+	}
+	if (prev) {
+		prev.className = prev.className.replace(/\bhighlight\b/, '');
+		prev = undefined;
+	}
+	if (event.target) {
+		prev = event.target;
+		prev.className += " highlight";
+	}
+}
+
+function tagClicked(event) {
+	if(prev) {
+		// Save the targeted info
+		bla = document.getElementsByTagName("link")[0].innerHTML;
+		alert(bla);
+		dict = {
+			"title": document.getElementsByTagName("title")[0].innerHTML,
+			"url": document.URL,
+			"htmlObject": event.target.innerHTML
+		};
+		chrome.storage.sync.set(dict, function(){
+			alert("successfully updated Google storage sync.");
+		});
+		
+		// Clean up the HTML (restore it to its original state)
+		// Remove the highlight class from the element we clicked on
+		prev.className = prev.className.replace(/\bhighlight\b/, '');
+		// Remove the event listeners
+		document.body.removeEventListener('mouseover', highlightHover);
+		document.body.removeEventListener('click', tagClicked);
+		// Remove our injected CSS
+		document.getElementById("dunfy-temporary-style").remove();
+	}
+}
