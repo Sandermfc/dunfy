02800e8d794e629d48fbfeace1f56b584b298e2f
Sandermfc <mathieu.robinson17@gmail.com>
2017-07-26 12:22:58 -0300

    google storage formatting change
    - Things in google storage are now stored as:
    {
    "url1": {
        "title": "this is page title for url1",
        "htmlObject": "<div>this is what the user clicked on</div>"
    }
    "url2": {
        "title": "this is page title for url2",
        "htmlObject": "<div>this is what the user clicked on</div>"
    }
    "urlx": {
        "title": "this is page title for urlx",
        "htmlObject": "<div>this is what the user clicked on</div>"
    }
    }
    
    - Every periodicCheck() now loops through every url.
    This commit introduces a bug where the notification does not appear 
    (even though the callback function is called).
    
    - getting the favicon from a url now works properly, simply use getDomFromUrl and pass in
    getFavicon(doc) as the callback function.

--- event.js
+++ event.js
@@ -8,17 +8,57 @@ chrome.alarms.onAlarm.addListener(function (alarm) {
 });
 
 function periodicCheck(){
-	var somethingChanged = true;
-	if(somethingChanged) {
-		chrome.notifications.create(
-		"", {
-			type: "basic",
-			title: "Primary Title",
-			message: "Primary message to display",
-			iconUrl: "icon.png"
-		},
-		function() {
-			//alert("notification created successfully");
+	chrome.storage.sync.get(null, function(items) { // "get(null.." will get every item in google storage
+		alert("get");
+		var value;
+		Object.keys(items).forEach(function(key) {
+			getDomFromUrl(key, function(doc){
+				alert("bla");
+				alert(key);
+				alert(items["title"]);
+				alert(items["htmlObject"]);
+				chrome.notifications.create(
+				"", {
+					type: "basic",
+					title: "title",
+					message: "message",
+					iconUrl: getFavicon(doc)
+				},
+				function() {
+					alert("notification created successfully");
+				});
+			});
 		});
+	});
+}
+
+function getFavicon(dom) {
+    var favicon = undefined;
+    var nodeList = dom.getElementsByTagName("link");
+    for (var i = 0; i < nodeList.length; i++)
+    {
+        if((nodeList[i].getAttribute("rel") == "icon")||(nodeList[i].getAttribute("rel") == "shortcut icon"))
+        {
+            favicon = nodeList[i].getAttribute("href");
+        }
+    }
+    return favicon;
+}
+
+function getDomFromUrl(url, callback) {
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', url, true);
+	xhr.responseType = 'document';
+	xhr.send();
+	xhr.onload = function(e) {
+		var doc = e.target.responseXML;
+		callback(doc);
 	}
+}
+
+function strip(html)
+{
+   var tmp = document.createElement("DIV");
+   tmp.innerHTML = html;
+   return tmp.textContent || tmp.innerText || "";
 }
