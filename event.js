a44cc42bf8568ab1f4edb61e39e89f73b9183552
Sandermfc <mathieu.robinson17@gmail.com>
2017-07-26 16:52:09 -0300

    Fixes to notifications
    Clicking a notification will now open showChanges.html
    In the future, add information on this page relative to the change.
    Possibly a way to save this page for future documentation?
    
    Also, getting an icon href is better than before

--- event.js
+++ event.js
@@ -1,5 +1,4 @@
-alert("event page called");
-
+alert("in event script");
 chrome.alarms.create("periodicCheck", {delayInMinutes: 0, periodInMinutes: 1}); // First time, execute immediately, then every minute.
 chrome.alarms.onAlarm.addListener(function (alarm) {
 	if(alarm.name === "periodicCheck"){
@@ -7,28 +6,34 @@ chrome.alarms.onAlarm.addListener(function (alarm) {
 	}
 });
 
+var debug = false;
+
 function periodicCheck(){
 	chrome.storage.sync.get(null, function(items) { // "get(null.." will get every item in google storage
-		alert("get");
-		var value;
-		Object.keys(items).forEach(function(key) {
-			getDomFromUrl(key, function(doc){
-				alert("bla");
-				alert(key);
-				alert(items["title"]);
-				alert(items["htmlObject"]);
-				chrome.notifications.create(
-				"", {
-					type: "basic",
-					title: "title",
-					message: "message",
-					iconUrl: getFavicon(doc)
-				},
-				function() {
-					alert("notification created successfully");
+		for(var key in items) {
+			if(items.hasOwnProperty(key)) {
+				getDomFromUrl(key, function(doc){
+					var iconHref = getFavicon(doc);
+					iconHref = formatHref(key, iconHref);
+					chrome.notifications.create(
+					"", {
+						type: "basic",
+						title: items[key]["title"],
+						message: strip(items[key]["htmlObject"]),
+						iconUrl: iconHref
+					},
+					function() {
+						if(debug){alert("notification successfully created");}
+					});
+					chrome.notifications.onClicked.addListener(function(){
+						chrome.tabs.create({ url: "showChanges.html" }, function(tab){
+							alert(tab["id"]);
+							//TODO: dynamically add a link towards the wanted page inside showChanges.html
+						});
+					});
 				});
-			});
-		});
+			}
+		}
 	});
 }
 
@@ -43,6 +48,40 @@ function getFavicon(dom) {
         }
     }
     return favicon;
+}
+
+function formatHref(url, href) {
+	// Remove leading '/' characters
+	while(href[0]=='/'){
+		href = href.slice(1, href.length);
+	}
+	//if http and www not found
+	if(href.search("http") == -1 && href.search("www") == -1) {
+		// alert("http and www not found in " + href);
+		dotFound = false;
+		posEnd = -1;
+		for(var i=0; i<url.length; i++) {
+			if(dotFound && url[i] == "/") {
+				posEnd = i;
+				break;
+			}
+			else if(url[i] == '.') {
+				dotFound = true;
+			}
+		}
+		// alert("/ found at position "+posEnd+" in "+url);
+		if(posEnd == -1) {
+			return url+"/"+href;
+		}
+		else{
+			url.slice(0, posEnd);
+			return url.slice(0, posEnd+1) + href;
+		}
+	}
+	if(href.search("http") != 0){ //http not at beginning
+		return "http://" + href;
+	}
+	return href;
 }
 
 function getDomFromUrl(url, callback) {
@@ -62,3 +101,5 @@ function strip(html)
    tmp.innerHTML = html;
    return tmp.textContent || tmp.innerText || "";
 }
+
+//https://stackoverflow.com/https://cdn.sstatic.net/Sites/stackoverflow/img/favicon.ico?v=4f32ecc8f43d
